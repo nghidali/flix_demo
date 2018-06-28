ad8988d5dc151a715a2f5c291bcb5d4594e18715
Natalie Ghidali <nghidali@fb.com>
2018-06-28 09:42:38 -0700

    finished basic movie table view loading content from movies api

---
+++ flix_demo/New Group/MovieCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieCell.h
+//  flix_demo
+//
+//  Created by Natalie Ghidali on 6/27/18.
+//  Copyright © 2018 Natalie Ghidali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCell : UITableViewCell
+
+@end
